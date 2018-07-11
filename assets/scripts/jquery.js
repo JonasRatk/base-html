073039ff181fc4a394d2fcd06ae0fba753a90a2f
DESKTOP-1R580GC\Jon <jonas.ratkevicius@outlook.com>
2018-07-11 12:23:49 +0300

    fullpage

--- assets/scripts/jquery.js
+++ assets/scripts/jquery.js
@@ -1,3 +1,75 @@
+$(document).ready(function () {
+    $('.timer').countTo();
+
+    $(document).ready(function () {
+        $('.owl-carousel').owlCarousel();
+    });
+
+    new WOW().init();
+
+    // $(".projects").isotope({
+    //        itemSelector: '.work-item',
+    //        layoutMode: 'fitRows',
+    //    });
+    //    
+    //    $('ul.filter li').click(function(){ 
+    //        
+    //      $("ul.filter li").removeClass("active");
+    //      $(this).addClass("active");        
+    //
+    //        var selector = $(this).attr('data-filter'); 
+    //        $(".projects").isotope({ 
+    //            filter: selector, 
+    //
+    //        }); 
+    //      return false; 
+    //    }); 
+
 $(document).ready(function(){
- $('.timer').countTo();
+	$('#menu').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+    
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 400) {
+            $(".header").css("background-color", "#0EB493");
+        } else {
+            $(".header").css("background-color", "rgba(16, 22, 54, 0.2)");
+        }
+    });
+    
+    
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
+})
+$(document).ready(function(){
+				$('.logo').click(function(){
+					$('.menu').toggleClass('show');
+				});
+			});
+    
+$('#back-top').click(function() {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
 });
